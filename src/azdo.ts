246e0b5bdc76968f02f0a3528e5c4fb86a4bae5a
Jannik Buschke <jannik.buschke@gmail.com>
2020-02-03 21:44:42 +0100

    wip

--- src/azdo.ts
+++ src/azdo.ts
@@ -2,3 +2,7 @@
 export const buildDirectory = process.env["Agent.BuildDirectory"]
 export const buildDirectory2 = process.env["Agent"]?.["BuildDirectory"]
 export const buildDirectory3 = process.env["Agent_BuildDirectory"]
+export const buildId = process.env["BUILD_BUILDID"]
+export const buildNumber = process.env["BUILD_BUILDNUMBER"]
+export const commit = process.env["BUILD_SOURCEVERSION"]
+export const time = process.env["SYSTEM_PIPELINESTARTTIME"]
